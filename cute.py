22f8558dc60eb1069ddd5d6ca940afa82e4401f3
eight <eight04@gmail.com>
2018-07-23 14:15:12 +0800

    Add: build readme server

--- cute.py
+++ cute.py
@@ -1,11 +1,18 @@
 #! python3
 
-from xcute import cute, exc
+from xcute import cute
 
+def readme():
+	"""Live reload readme"""
+	from livereload import Server
+	server = Server()
+	server.watch("README.rst", "py cute.py readme_build")
+	server.serve(open_url_delay=1, root="build/readme")
+	
 cute(
 	pkg_name = "node_vm2",
 	lint = [
-		'eslint **/*.js --ignore-path .gitignore',
+		'cd node_vm2/vm-server && npm test && cd ..',
 		'pylint {pkg_name}'
 	],
 	test = ['lint', 'python test.py', 'readme_build'],
@@ -26,13 +33,11 @@ cute(
 	install = 'pip install -e .',
 	install_err = 'elevate -c -w pip install -e .',
 	readme_build = [
-		'python setup.py --long-description > build/long-description.rst',
-		'rst2html --no-raw --exit-status=1 --verbose '
-			'build/long-description.rst build/long-description.html'
+		'python setup.py --long-description | x-pipe build/readme/index.rst',
+		'rst2html5.py --no-raw --exit-status=1 --verbose '
+			'build/readme/index.rst build/readme/index.html'
 	],
-	readme_build_err = ['readme_show', exc],
-	readme_show = 'start build/long-description.html',
-	readme = 'readme_build',
-	readme_post = 'readme_show',
+	readme_pre = "readme_build",
+	readme = readme,
 	doc = 'sphinx-autobuild -B -z {pkg_name} docs docs/build'
 )
